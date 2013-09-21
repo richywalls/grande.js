6dceb602736cc7d145f040a1e0de1a6f4f411f6d
Ricardo <rpt@nomail.com>
2013-09-21 00:19:40 -0300

    wip angularize

--- js/grande.js
+++ js/grande.js
@@ -1,47 +1,234 @@
+function getParent(node, condition, returnCallback) {
+  while (node.parentNode) {
+    if (condition(node)) {
+      return returnCallback(node);
+    }
+    node = node.parentNode;
+  }
+}
+
+function getParentWithTag(node, nodeType) {
+  var checkNodeType = function(node) {
+    console.log(node.nodeName);
+    return node.nodeName.toLowerCase() === nodeType;
+  },
+    returnNode = function(node) {
+      return node;
+    };
+
+  return getParent(node, checkNodeType, returnNode);
+}
+
+function hasParentWithTag(node, nodeType) {
+  return !!getParentWithTag(node, nodeType);
+}
+
+
+function getParentHref(node) {
+  var checkHref = function(node) {
+    return typeof node.href !== "undefined";
+  },
+    returnHref = function(node) {
+      return node.href;
+    };
+
+  return getParent(node, checkHref, returnHref);
+}
+
+angular.module('project', []).
+directive('grande', function($compile) {
+  // Runs during compile
+  return {
+
+    // name: '',
+    // priority: 1,
+    // terminal: true,
+    // scope: {}, // {} = isolate, true = child, false/undefined = no change
+    // cont­rol­ler: function($scope, $element, $attrs, $transclue) {},
+    // require: 'ngModel', // Array = multiple requires, ? = optional, ^ = check parent elements
+    restrict: 'A', // E = Element, A = Attribute, C = Class, M = Comment
+    //template: '<div><button ng-click="visible = !visible">hola</button><button ng-click="refresh()">refresh</button></div>',
+    // template: '',
+    // templateUrl: '',
+    // replace: true,
+    // transclude: true,
+    // compile: function(tElement, tAttrs, function transclude(function(scope, cloneLinkingFn){ return function linking(scope, elm, attrs){}})),
+    link: function(scope, iElm, iAttrs, controller) {
+      function triggerTextSelection() {
+        var selectedText = document.getSelection();
+        // The selected text is collapsed, push the menu out of the way
+        if (selectedText.isCollapsed) {
+          scope.$apply(function(){
+            scope.visible = false;  
+          });
+          scope.textMenu.css('top', '-999px');
+          scope.textMenu.css('left', '-999px');
+        } else {
+          var clientRectBounds = selectedText.getRangeAt(0).getBoundingClientRect();         
+          scope.$apply(function(){
+            scope.visible = true;
+          });
+          //TODO: Every time we show the menu, reload the state
+          //reloadMenuState();
+          scope.refresh();
+          scope.textMenu.css('top', (clientRectBounds.top - 5 + window.pageYOffset)+'px');
+          scope.textMenu.css('left', ((clientRectBounds.left + clientRectBounds.right) / 2)+'px');
+
+        }
+      }
+
+      scope.refresh = function() {
+        var focusNode = window.getSelection().focusNode;
+        if (focusNode){
+          scope.bold.active = false;
+          while (focusNode.parentNode) {
+            focusNode = focusNode.parentNode;
+            if (focusNode.nodeName === 'B')
+              scope.bold.active = true;
+            scope.$apply();
+            console.log(focusNode.nodeName)
+          }
+        }
+      };
+      scope.visible = false;
+      scope.isurl = false;
+      scope.toggleUrl = function() {
+        scope.textMenu.css('top', '150px');
+        scope.textMenu.css('left', '150px');
+        //scope.isurl = !scope.isurl;
+      };
+      scope.bold = {
+        active: false,
+        toggle: function() {
+          var sel = window.getSelection(),
+            selNode = sel.anchorNode;
+          document.execCommand('bold', false);
+          this.active = !this.active;
+        }
+      };
+      scope.toggleItalic = function() {
+        document.execCommand('bold', false);
+        console.log('toggleItalic');
+      };
+      scope.toggleHeader1 = function() {
+        document.execCommand('bold', false);
+        console.log('toggleHeader1');
+      };
+      scope.toggleHeader2 = function() {
+        document.execCommand('bold', false);
+        console.log('toggleHeader2');
+      };
+      scope.toggleQuote = function() {
+        document.execCommand('bold', false);
+        console.log('toggleQuote');
+      };
+      scope.textMenu = angular.element("<div class='text-menu' ng-show='visible'><div class='options' ng-class=\"{'url-mode':isurl}\"><span class='no-overflow'>");
+      var inputs = angular.element("<span class='ui-inputs'>");
+      scope.textMenu.children().children().append(inputs);
+      inputs.append("<button class='bold ' ng-click='bold.toggle()' ng-class=\"{'active':bold.active}\">B</button>");
+      inputs.append("<button class='italic' ng-click='toggleItalic()'>i</button>");
+      inputs.append("<button class='header1' ng-click='toggleHeader1()'>h1</button>");
+      inputs.append("<button class='header2' ng-click='toggleHeader2()'>h2</button>");
+      inputs.append("<button class='quote' ng-click='toggleQuote()'>&rdquo;</button>");
+      inputs.append("<button class='url useicons' ng-click='toggleUrl()'>&#xe001;</button>");
+      scope.urlInput = angular.element("<input class='url-input' type='text' placeholder='Paste or type a link' ng-blur='triggerUrlBlur()' ng-keydown='triggerUrlSet()'/>");
+      inputs.append(scope.urlInput);
+      iElm.after(scope.textMenu);
+      var test = angular.element('<pre>visible: {{visible}}</pre>');
+      $compile(test)(scope);
+      iElm.after(test);
+      $compile(scope.textMenu)(scope);
+
+        var node;
+
+        // Trigger on both mousedown and mouseup so that the click on the menu
+        // feels more instantaneously active
+        document.onmousedown = triggerTextSelection;
+        document.onmouseup = function(event) {
+          setTimeout(function() {
+            triggerTextSelection(event);
+          }, 1);
+        };
+
+        //TODO
+        //document.onkeydown = preprocessKeyDown;
+
+        /*document.onkeyup = function(event) {
+          var sel = window.getSelection();
+
+          // FF will return sel.anchorNode to be the parentNode when the triggered keyCode is 13
+          if (sel.anchorNode && sel.anchorNode.nodeName !== "ARTICLE") {
+            triggerNodeAnalysis(event);
+
+            if (sel.isCollapsed) {
+              triggerTextParse(event);
+            }
+          }
+        };*/
+
+        // Handle window resize events
+        document.onresize = triggerTextSelection;
+
+        scope.triggerUrlBlur = function(){
+          console.log('TODO:triggerUrlBlur');
+        }
+        scope.triggerUrlSet = function(){
+         console.log('TODO:triggerUrlSet'); 
+        }
+        //scope.urlInput.bind('onblur',triggerUrlBlur);
+        //scope.urlInput.bind('onkeydown', triggerUrlSet);
+
+        iElm.contentEditable = true;
+        iElm.onmousedown = iElm.onkeyup = iElm.onmouseup = triggerTextSelection;
+
+
+
+      grande.bind(document.querySelectorAll("article"), null, scope);
+    }
+  };
+});
+
+
 (function() {
   var EDGE = -999;
 
-  var root = this,   // Root object, this is going to be the window for now
-      document = this.document, // Safely store a document here for us to use
-      editableNodes = document.querySelectorAll(".g-body article"),
-      isFirefox = navigator.userAgent.toLowerCase().indexOf('firefox') > -1,
-      options = {
-        animate: true
-      },
-      textMenu,
-      optionsNode,
-      urlInput,
-      previouslySelectedText,
+  var root = this, // Root object, this is going to be the window for now
+    document = this.document, // Safely store a document here for us to use
+    editableNodes = document.querySelectorAll(".g-body article"),
+    isFirefox = navigator.userAgent.toLowerCase().indexOf('firefox') > -1,
+    options = {
+      animate: true
+    },
+    textMenu,
+    optionsNode,
+    previouslySelectedText,
 
-      grande = {
-        bind: function(bindableNodes, opts) {
-          if (bindableNodes) {
-            editableNodes = bindableNodes;
-          }
-
-          attachToolbarTemplate();
-          bindTextSelectionEvents();
-          bindTextStylingEvents();
-
-          options = opts || options;
-        },
-        select: function() {
-          triggerTextSelection();
+    grande = {
+      bind: function(bindableNodes, opts, scope) {
+        if (bindableNodes) {
+          editableNodes = bindableNodes;
         }
+
+        bindTextStylingEvents();
+        options = opts || options;
       },
+      select: function() {
+        triggerTextSelection();
+      }
+    },
 
-      tagClassMap = {
-        "b": "bold",
-        "i": "italic",
-        "h1": "header1",
-        "h2": "header2",
-        "a": "url",
-        "blockquote": "quote"
-      };
+    tagClassMap = {
+      "b": "bold",
+      "i": "italic",
+      "h1": "header1",
+      "h2": "header2",
+      "a": "url",
+      "blockquote": "quote"
+    };
 
-  function attachToolbarTemplate() {
+  /*function attachToolbarTemplate() {
     var div = document.createElement("div"),
-        toolbarTemplate = "<div class='options'> \
+      toolbarTemplate = "<div class='options'> \
           <span class='no-overflow'> \
             <span class='ui-inputs'> \
               <button class='bold'>B</button> \
@@ -65,55 +252,13 @@
     textMenu = document.querySelectorAll(".text-menu")[0];
     optionsNode = document.querySelectorAll(".text-menu .options")[0];
     urlInput = document.querySelectorAll(".text-menu .url-input")[0];
-  }
-
-  function bindTextSelectionEvents() {
-    var i,
-        len,
-        node;
-
-    // Trigger on both mousedown and mouseup so that the click on the menu
-    // feels more instantaneously active
-    document.onmousedown = triggerTextSelection;
-    document.onmouseup = function(event) {
-      setTimeout(function() {
-        triggerTextSelection(event);
-      }, 1);
-    };
-
-    document.onkeydown = preprocessKeyDown;
-
-    document.onkeyup = function(event){
-      var sel = window.getSelection();
-
-      // FF will return sel.anchorNode to be the parentNode when the triggered keyCode is 13
-      if (sel.anchorNode && sel.anchorNode.nodeName !== "ARTICLE") {
-        triggerNodeAnalysis(event);
-
-        if (sel.isCollapsed) {
-          triggerTextParse(event);
-        }
-      }
-    };
-
-    // Handle window resize events
-    root.onresize = triggerTextSelection;
-
-    urlInput.onblur = triggerUrlBlur;
-    urlInput.onkeydown = triggerUrlSet;
-
-    for (i = 0, len = editableNodes.length; i < len; i++) {
-      node = editableNodes[i];
-      node.contentEditable = true;
-      node.onmousedown = node.onkeyup = node.onmouseup = triggerTextSelection;
-    }
-  }
+  }*/
 
   function iterateTextMenuButtons(callback) {
     var textMenuButtons = document.querySelectorAll(".text-menu button"),
-        i,
-        len,
-        node;
+      i,
+      len,
+      node;
 
     for (i = 0, len = textMenuButtons.length; i < len; i++) {
       node = textMenuButtons[i];
@@ -125,11 +270,11 @@
   }
 
   function bindTextStylingEvents() {
-    iterateTextMenuButtons(function(node) {
+    /*iterateTextMenuButtons(function(node) {
       node.onmousedown = function(event) {
         triggerTextStyling(node);
       };
-    });
+    });*/
   }
 
   function getFocusNode() {
@@ -138,11 +283,11 @@
 
   function reloadMenuState() {
     var className,
-        focusNode = getFocusNode(),
-        tagClass,
-        reTag;
+      focusNode = getFocusNode(),
+      tagClass,
+      reTag;
 
-    iterateTextMenuButtons(function(node) {
+    /*iterateTextMenuButtons(function(node) {
       className = node.className;
 
       for (var tag in tagClassMap) {
@@ -159,19 +304,18 @@
           break;
         }
       }
-    });
+    });*/
   }
 
   function preprocessKeyDown(event) {
     var sel = window.getSelection(),
-        parentParagraph = getParentWithTag(sel.anchorNode, "p"),
-        p,
-        isHr;
+      parentParagraph = getParentWithTag(sel.anchorNode, "p"),
+      p,
+      isHr;
 
     if (event.keyCode === 13 && parentParagraph) {
       prevSibling = parentParagraph.previousSibling;
-      isHr = prevSibling && prevSibling.nodeName === "HR" &&
-        !parentParagraph.textContent.length;
+      isHr = prevSibling && prevSibling.nodeName === "HR" && !parentParagraph.textContent.length;
 
       // Stop enters from creating another <p> after a <hr> on enter
       if (isHr) {
@@ -182,8 +326,8 @@
 
   function triggerNodeAnalysis(event) {
     var sel = window.getSelection(),
-        anchorNode,
-        parentParagraph;
+      anchorNode,
+      parentParagraph;
 
     if (event.keyCode === 13) {
 
@@ -202,14 +346,14 @@
 
   function insertHorizontalRule(parentParagraph) {
     var prevSibling,
-        prevPrevSibling,
-        hr;
+      prevPrevSibling,
+      hr;
 
     prevSibling = parentParagraph.previousSibling;
     prevPrevSibling = prevSibling;
-    
-    while(prevPrevSibling = prevPrevSibling.previousSibling){
-    	if (prevPrevSibling.nodeType != Node.TEXT_NODE) break;
+
+    while (prevPrevSibling = prevPrevSibling.previousSibling) {
+      if (prevPrevSibling.nodeType != Node.TEXT_NODE) break;
     }
 
     if (prevSibling.nodeName === "P" && !prevSibling.textContent.length && prevPrevSibling.nodeName !== "HR") {
@@ -235,7 +379,7 @@
 
   function insertListOnSelection(sel, textProp, listType) {
     var execListCommand = listType === "ol" ? "insertOrderedList" : "insertUnorderedList",
-        nodeOffset = listType === "ol" ? 3 : 2;
+      nodeOffset = listType === "ol" ? 3 : 2;
 
     document.execCommand(execListCommand);
     sel.anchorNode[textProp] = sel.anchorNode[textProp].substring(nodeOffset);
@@ -245,13 +389,13 @@
 
   function triggerTextParse(event) {
     var sel = window.getSelection(),
-        textProp,
-        subject,
-        insertedNode,
-        unwrap,
-        node,
-        parent,
-        range;
+      textProp,
+      subject,
+      insertedNode,
+      unwrap,
+      node,
+      parent,
+      range;
 
     // FF will return sel.anchorNode to be the parentNode when the triggered keyCode is 13
     if (!sel.isCollapsed || !sel.anchorNode || sel.anchorNode.nodeName === "ARTICLE") {
@@ -270,8 +414,8 @@
     }
 
     unwrap = insertedNode &&
-            ["ul", "ol"].indexOf(insertedNode.nodeName.toLocaleLowerCase()) >= 0 &&
-            ["p", "div"].indexOf(insertedNode.parentNode.nodeName.toLocaleLowerCase()) >= 0;
+      ["ul", "ol"].indexOf(insertedNode.nodeName.toLocaleLowerCase()) >= 0 &&
+      ["p", "div"].indexOf(insertedNode.parentNode.nodeName.toLocaleLowerCase()) >= 0;
 
     if (unwrap) {
       node = sel.anchorNode;
@@ -292,17 +436,17 @@
 
   function triggerTextStyling(node) {
     var className = node.className,
-        sel = window.getSelection(),
-        selNode = sel.anchorNode,
-        tagClass,
-        reTag;
+      sel = window.getSelecti7on(),
+      selNode = sel.anchorNode,
+      tagClass,
+      reTag;
 
     for (var tag in tagClassMap) {
       tagClass = tagClassMap[tag];
       reTag = new RegExp(tagClass);
 
       if (reTag.test(className)) {
-        switch(tag) {
+        switch (tag) {
           case "b":
             if (selNode && !hasParentWithTag(selNode, "h1") && !hasParentWithTag(selNode, "h2")) {
               document.execCommand(tagClass, false);
@@ -383,56 +527,6 @@
 
       urlInput.focus();
     }, 150);
-  }
-
-  function getParent(node, condition, returnCallback) {
-    while (node.parentNode) {
-      if (condition(node)) {
-        return returnCallback(node);
-      }
-
-      node = node.parentNode;
-    }
-  }
-
-  function getParentWithTag(node, nodeType) {
-    var checkNodeType = function(node) { return node.nodeName.toLowerCase() === nodeType; },
-        returnNode = function(node) { return node; };
-
-    return getParent(node, checkNodeType, returnNode);
-  }
-
-  function hasParentWithTag(node, nodeType) {
-    return !!getParentWithTag(node, nodeType);
-  }
-
-  function getParentHref(node) {
-    var checkHref = function(node) { return typeof node.href !== "undefined"; },
-        returnHref = function(node) { return node.href; };
-
-    return getParent(node, checkHref, returnHref);
-  }
-
-  function triggerTextSelection() {
-      var selectedText = root.getSelection(),
-          range,
-          clientRectBounds;
-
-      // The selected text is collapsed, push the menu out of the way
-      if (selectedText.isCollapsed) {
-        setTextMenuPosition(EDGE, EDGE);
-        textMenu.className = "text-menu hide";
-      } else {
-        range = selectedText.getRangeAt(0);
-        clientRectBounds = range.getBoundingClientRect();
-
-        // Every time we show the menu, reload the state
-        reloadMenuState();
-        setTextMenuPosition(
-          clientRectBounds.top - 5 + root.pageYOffset,
-          (clientRectBounds.left + clientRectBounds.right) / 2
-        );
-      }
   }
 
   function setTextMenuPosition(top, left) {
